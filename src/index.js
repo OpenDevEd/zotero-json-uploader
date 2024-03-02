c1490481d2e6b94023b042bf0208de2e3668464e
bjohas <bjohas+git@gmail.com>
2024-03-02 11:02:24 +0000

    Update to src/index.js

--- src/index.js
+++ src/index.js
@@ -18,31 +18,31 @@ const zotero = new Zotero({ group_id: group });
 // console.log(typeof files[0]);
 
 
+/**
+ * 
+ * @param {string} infile 
+ */
 async function main(infile) {
+    /** @type {string} */
     const outf = infile + ".zotero.json";
+    /** @type {object} */
     const inob = JSON.parse(fs.readFileSync(infile, 'utf8'));
-    await jq.run(filter,
+    /** @type {object} */
+    const data = await jq.run(filter,
         inob,
-        { input: 'json', output: 'pretty' }).then(
-            (data) => {
-                // write data to file
-                fs.writeFileSync(outf, data);
-            }
-        );
+        { input: 'json', output: 'pretty' });
+    /** @type {void} */
+    fs.writeFileSync(outf, data);
+    /** @type {object} */
     const result = await zotero.create_item({ files: [outf] });
+    /** @type {void} */
     fs.writeFileSync(infile + ".zotero-result.json", JSON.stringify(result));
     // const result = JSON.parse(fs.readFileSync(infile + ".zotero-result.json", 'utf8'));
     // console.log("data: " + JSON.stringify(result, null, 4));
-    const zotobject = await jq.run(".[] | .successful | [ to_entries[] | .value.data ] ", result, { input: 'json', output: 'json' }).then(
-        (data) => {
-            return data;
-        }
-    );
-    const openalexobject = await jq.run('.results | [ .[] | { "key": .id, "value": . } ] | from_entries', inob, { input: 'json', output: 'json' }).then(
-        (data) => {
-            return data;
-        }
-    );
+    /** @type {object} */
+    const zotobject = await jq.run(".[] | .successful | [ to_entries[] | .value.data ] ", result, { input: 'json', output: 'json' });
+    /** @type {object} */
+    const openalexobject = await jq.run('.results | [ .[] | { "key": .id, "value": . } ] | from_entries', inob, { input: 'json', output: 'json' });
     // console.log("data: " + JSON.stringify(data));
     const tempdir = "temp";
     // create directory if it doesn't exist
@@ -54,7 +54,9 @@ async function main(infile) {
         const oakey = s.callNumber.replace(/openalex\:/g, '');
         // console.log(s.key + " => " + JSON.stringify("https://openalex.org/" + oaob[oakey], null, 4));
         const writefile = tempdir + "/" + oakey + ".json";
+        /** @type {void} */
         fs.writeFileSync(writefile, JSON.stringify(openalexobject["https://openalex.org/" + oakey], null, 4));
+        /** @type {object} */
         const result = await zotero.item({  key: s.key, addfiles: [writefile], addtags: ["openalex:yes"] });
     };
 
