377d528587f69a01686685996fe60102813d649a
bjohas <bjohas+git@gmail.com>
2024-06-10 13:11:37 +0100

    Update to test3.sh

--- tests/test3.sh
+++ tests/test3.sh
@@ -6,5 +6,5 @@ do
     echo "Processing: $data_source"
     gzip -d "$data_source"
     unzipped_file="${data_source%.gz}"
-    npm start -- db-upload "$unzipped_file"
+    zotero-json-uploader db-upload "$unzipped_file"
 done
